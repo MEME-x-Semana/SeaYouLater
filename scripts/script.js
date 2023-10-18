686be2a7372f9d724af12a78a4c9994d502957dd
axelsat <71181769+AxelCriado@users.noreply.github.com>
2023-10-18 18:48:08 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,28 @@
 // Barra de navegación
 
+document.addEventListener("DOMContentLoaded", function () {
+  const sliderContainer = document.querySelector(".slider-container");
+  const nextNav = document.querySelector(".slider-nav-next");
+  const prevNav = document.querySelector(".slider-nav-prev");
+  const interval = 2000;
+  let slideTimer;
 
+  function startSlideTimer() {
+    slideTimer = setInterval(() => {
+      nextNav.click();
+    }, interval);
+  }
+
+  function stopSlideTimer() {
+    clearInterval(slideTimer);
+  }
+
+  startSlideTimer();
+
+  sliderContainer.addEventListener("mouseover", stopSlideTimer);
+
+  sliderContainer.addEventListener("mouseout", startSlideTimer);
+});
 
 
 //Menu Desplegable
